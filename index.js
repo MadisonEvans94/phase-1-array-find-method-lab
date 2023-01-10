2122d8c95272b395bb041f67f156cc2b1806eafc
Madison Evans <m.corbinevans@gmail.com>
2023-01-09 21:07:35 -0700

    passed lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+const record = [
+	{ year: "2015", result: "B" },
+	{ year: "2014", result: "N/A" },
+	{ year: "2013", result: "L" },
+	//...
+];
+
+const superbowlWin = (record) => {
+	const output = record.find((entry) => entry.result === "W");
+	if (output) {
+		return output.year;
+	} else {
+		return output;
+	}
+};
